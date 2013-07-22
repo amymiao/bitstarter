b4f653e33db3e75ae5f1414e466bb0fb87d2af13
Ubuntu <ubuntu@ip-172-31-3-37.us-west-2.compute.internal>
2013-07-22 21:25:47 +0000

    Adding Index.html

--- web.js
+++ web.js
@@ -1,9 +1,27 @@
+#!/usr/bin/env node
+
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+
+
+/*fs.readFile('./Index.html',function read(err,data){
+    if (err) {
+	throw err;
+	}
+    console.log(data);
+});*/
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fs=require('fs');
+    var buffer=new Buffer();
+    response.send(buffer.toString('utc-8', fs.readFireSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
